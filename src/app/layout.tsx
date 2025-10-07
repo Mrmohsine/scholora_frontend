1704cf36731a8632eff9e4876cce44be092284b7
OBouysfi <o.bouysfi@byteit.ma>
2025-10-07 12:28:43 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ 
@@ -62,6 +63,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8M27DPP48Y"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8M27DPP48Y', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body 
         className="font-sans antialiased" 
         suppressHydrationWarning={true}
